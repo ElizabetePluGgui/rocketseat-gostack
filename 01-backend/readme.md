e59769aadad02e15088e82296155f230d64cfdf6
Elizabete Fabri <elizabete958@gmail.com>
2021-01-31 20:53:03 -0300

    Backend

---
+++ 01-backend/readme.md
@@ -0,0 +1,15 @@
+# Iniciando o projeto BACKEND
+
+# Comandos de instalações do git ou prompt.
+
+## Criar uma pasta com o nome de backend e executar os seguintes comandos:
+
+### Passo 1
+
+cd backend
+yarn init -y
+Criar uma pasta SRC e um documento com o nome de index.js
+
+### Passo 2
+
+yarn add express
---
+++ 01-backend/src/index.js
@@ -0,0 +1,14 @@
+// Para visualiza um HELLO WORLD em tela
+// Importando as variaves dentro doEXPRESS
+const express = require('express');
+
+const app = express();
+
+// Primeiro parametro é project(rota do caminho) 2 é uma função que recebe dois parametros.
+// A 1 é a requisão e a 2 é a resposta. E ela precisa retornar alguma coisa:
+// Todo metodo da Rota GET ela precisa retornar RESPONSE pq é ele que vai dar a resposta para o FRONTEND
+app.get('/projects', (request, response) => {});
+
+// Como acessar a aplicação pelo navegador? Precisa ouvir uma porta no navegador.
+// Pelo localhost:3333 app é a variavel declarada (3333)É a porta.
+app.listen(3333);
