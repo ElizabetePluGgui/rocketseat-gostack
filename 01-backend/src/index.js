a691d8d0c55929778b60dd18d4f166c3746f85f7
Elizabete Fabri <elizabete958@gmail.com>
2021-01-31 21:24:33 -0300

    Backend

--- 01-backend/src/index.js
+++ 01-backend/src/index.js
@@ -7,7 +7,9 @@ const app = express();
 // Primeiro parametro é project(rota do caminho) 2 é uma função que recebe dois parametros.
 // A 1 é a requisão e a 2 é a resposta. E ela precisa retornar alguma coisa:
 // Todo metodo da Rota GET ela precisa retornar RESPONSE pq é ele que vai dar a resposta para o FRONTEND
-app.get('/projects', (request, response) => {});
+app.get('/projects', (request, response) => {
+  return response.json({ message: 'Hello Elizabete' });
+});
 
 // Como acessar a aplicação pelo navegador? Precisa ouvir uma porta no navegador.
 // Pelo localhost:3333 app é a variavel declarada (3333)É a porta.
