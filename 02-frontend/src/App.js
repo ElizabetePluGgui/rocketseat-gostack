90e578906bcabf2ee8fd8a8fb3afc39bba0ed8f7
Elizabete Fabri <elizabete958@gmail.com>
2021-02-01 17:40:59 -0300

    ok

--- 02-frontend/src/App.js
+++ 02-frontend/src/App.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
+
 import api from './services/api';
 
 import './App.css';
-import backgroundImage from './assets/background.jpg';
+import backgroundImage from '../src/assets/background.jpg';
+
 import Header from './components/Header';
 
 /**
@@ -12,32 +14,23 @@ import Header from './components/Header';
  */
 
 function App() {
-  const [projects, setProjects] = useState([]);
-
-  useEffect(() => {
-    api.get('projects').then((response) => {
-      setProjects(response.data);
-    });
-  }, []);
-
-  async function handleAddProject() {
-    // setProjects([...projects, `Novo projeto ${Date.now()}`]);
-
-    const response = await api.post('projects', {
-      title: `Novo projeto ${Date.now()}`,
-      owner: 'Diego Fernandes',
-    });
-
-    const project = response.data;
-
-    setProjects([...projects, project]);
+  const [projects, setProjects] = useState([
+    'Desenvolvimento de app',
+    'Front-end web',
+  ]);
+  // useState retorna um array com 2 posições
+  //
+  // 1. Variável com seu valor inicial
+  // 2. Função para atualizarmos esse valor
+  function handleAddProject() {
+    setProjects([...projects, `Novo Projeto ${Date.now()}`]);
   }
 
   return (
     <>
       <Header title="Projects" />
 
-      <img src={backgroundImage} />
+      <img width={500} src={backgroundImage} />
 
       <ul>
         {projects.map((project) => (
