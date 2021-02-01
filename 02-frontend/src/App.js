5e473770fdac4034b405015efd3cf3793da5156a
Elizabete Fabri <elizabete958@gmail.com>
2021-02-01 16:00:54 -0300

    ok

--- 02-frontend/src/App.js
+++ 02-frontend/src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 function App() {
+  const projects = useState(['Desenvolvimento de app', 'Front-end web']);
+
+  function handleAddProject() {
+    projects.push(`Novo projeto ${Date.now()}`);
+    console.log(projects);
+  }
   return (
     <>
-      <Header title="Elizabete de Sousa Fabri" />
-      <Header />
+      <Header title="Projects" />
+      <ul>
+        {projects.map((project) => (
+          <li key={project}>{project}</li>
+        ))}
+      </ul>
+
+      <button type="button" onclick={handleAddProject}>
+        Adicionar projeto
+      </button>
     </>
   );
 }
