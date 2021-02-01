409b560d6d84141ffa569fefc1cce8e96ea65997
Elizabete Fabri <elizabete958@gmail.com>
2021-02-01 00:50:07 -0300

    ok

--- 02-frontend/webpack.config.js
+++ 02-frontend/webpack.config.js
@@ -6,13 +6,29 @@ module.exports = {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+  },
   module: {
-    rules: {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
       },
-    },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+      {
+        test: /.*\.(gif|png|jpe?g)$/i,
+        use: {
+          loader: 'file-loader',
+        },
+      },
+    ],
   },
 };
